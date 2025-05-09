25dc2ffec56fc1ccfeb9d3122bb01d23674c87aa
imuzi <1103573279@qq.com>
2025-05-09 17:58:20 +0800

    3213331

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from pkg.plugin.events import *
 from .autodl_client import AutoDLClient
 from .models import AutoDLConfig, GrabConfig, GrabMenuData, Instance
 from .storage import UserStorage
-# import threading
-# import time
-# import logging
-# from typing import Dict, List, Optional, Any
+import threading
+import time
+import logging
+from typing import Dict, List, Optional, Any
 
 @register(name="AutoDLPlugin", description="AutoDL监控与抢卡助手", version="1.0.0", author="YourName")
 class HelloPlugin(BasePlugin):
